afd55c669862e3a147b44897607a54d5b8e2ed94
Luke Bakken <lrbakken@amazon.com>
2025-11-07 09:02:45 -0800

    Add rebar3 project

---
+++ rebar3/README.md
@@ -0,0 +1,9 @@
+amqp
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ rebar3/src/amqp_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc amqp public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(amqp_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    amqp_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
